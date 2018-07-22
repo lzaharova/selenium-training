61eb5cf4ba9b8ef5574bb896b026cbc9388f8299
Larisa Zakharova <zakharoval78@gmail.com>
2018-07-22 14:36:04 +0300

    2

---
+++ java-example/src/test/java/ru/stqa/training/selenium/Login.java
@@ -0,0 +1,41 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+
+public class Login {
+
+        private  WebDriver driver;
+
+    public Login() {
+    }
+
+    @Before
+        public void start() {
+            driver = new ChromeDriver();
+           driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        }
+    @Test
+        public void login() {
+            driver.get("http://localhost/litecart/admin/login.php");
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.name("login")).click();
+        }
+
+    @After
+        public void stop() {
+            driver.quit();
+            driver = null;
+        }
+
+    }
+
+
